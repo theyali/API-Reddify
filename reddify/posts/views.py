fce3c915ea4776cf3791f83067fb7a2f6dc017bc
theyali <haciyev.ali@hotmail.com>
2023-08-03 21:07:00 +0400

    Votes and Posts done

--- reddify/posts/views.py
+++ reddify/posts/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework import generics, permissions, mixins, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from .models import Post, Vote
+from .serializers import PostSerializer, VoteSerializer
 
-# Create your views here.
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+class VoteCreate(generics.CreateAPIView , mixins.DestroyModelMixin):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+    
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError("You have already voted for this post!")
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError("You never voted for this post!")
