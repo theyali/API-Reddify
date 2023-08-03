f2df61997ab094236d6c7b55d48bd92e6509c061
theyali <haciyev.ali@hotmail.com>
2023-08-03 21:22:01 +0400

    Done Test Project

--- reddify/posts/views.py
+++ reddify/posts/views.py
@@ -13,6 +13,20 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
 
+class PostEdit(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], poster = self.request.user)
+
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError("You have not permission to delete this post")
+
+
 class VoteCreate(generics.CreateAPIView , mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
